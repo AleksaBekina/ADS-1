95f22321680fb9f5d1bd335ada0127fcc85cb942
AleksaBekina <116704236+AleksaBekina@users.noreply.github.com>
2023-02-25 23:56:25 +0300

    Update

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,36 +3,36 @@
 #include "alg.h"
 
 bool checkPrime(uint64_t value) {
-	uint64_t del = 2;
-	while (del * del <= value) {
-		if (value % del == 0) return false;
-		del++;
-	}
-	return true;
+    uint64_t del = 2;
+    while (del * del <= value) {
+        if (value % del == 0) return false;
+            del++;
+        }
+        return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-	uint64_t index = 0;
-	uint64_t number = 2;
-	while (index < n) {
-		if (checkPrime(number++)) index++;
-	}
-	return number - 1;
+    uint64_t index = 0;
+    uint64_t number = 2;
+    while (index < n) {
+        if (checkPrime(number++)) index++;
+    }
+    return number - 1;
 }
 
 uint64_t nextPrime(uint64_t value) {
-	while (!checkPrime(++value)) {};
-	return value;
+    while (!checkPrime(++value)) {};
+    return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-	uint64_t number = 2;
-	uint64_t sum = 0;
-	while (number < hbound) {
-		if (checkPrime(number)) {
-			sum += number;
-		}
-		number++;
-	}
-	return sum;
+    uint64_t number = 2;
+    uint64_t sum = 0;
+    while (number < hbound) {
+        if (checkPrime(number)) {
+            sum += number;
+        }
+        number++;
+    }
+    return sum;
 }
